190b0c1049e75a5ec6ac2637493eae6e0df4c778
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2020-07-07 23:17:05 -0500

    Data Journalism and D3

---
+++ assets/js/app.js
@@ -0,0 +1,146 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 600;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(healthData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.95,
+      d3.max(healthData, d => d[chosenXAxis]) * 1.05
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]));
+  
+    return circlesGroup;
+  }
+  
+
+/*
+Columns in the csv file
+id,state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,
+healthcare,healthcareLow,healthcareHigh,
+obesity,obesityLow,obesityHigh,
+smokes,smokesLow,smokesHigh,
+-0.385218228 <-- ? What???
+
+*/
+
+d3.csv("assets/data/data.csv").then((healthData, err) => {
+    if (err) throw err;
+    // parse data
+    healthData.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        data.age = +data.age;
+    });
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(healthData, chosenXAxis);
+
+    // Create y scale function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.healthcare)*1.05])
+        .range([height, 0]);
+
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("g")
+        .attr("transform", d => `translate(${xLinearScale(d[chosenXAxis])}, ${yLinearScale(d.healthcare)})`);
+
+    circlesGroup
+        .append("circle")
+        .classed("stateCircle", true)
+        .attr("r", 15)
+        .attr("opacity", ".5");
+
+    circlesGroup
+        .append("text")
+        .classed("stateText", true)
+        .attr("dy", 5)
+        .text(d => d.abbr);
+
+    // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var healthLengthLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "poverty") // value to grab for event listener
+        .classed("active", true)
+        .text("In Poverty (%)");
+
+    // append y axis
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("axis-text", true)
+        .text("Lacks Healthcare (%)");
+
+});
